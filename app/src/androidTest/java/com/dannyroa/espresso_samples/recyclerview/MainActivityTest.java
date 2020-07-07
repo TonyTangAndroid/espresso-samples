b4d4b3c9537e6ebe63a1c1cbbc1e11478e6005f1
Tony Tang <tonytang@uber.com>
2020-07-06 19:16:25 -0700

    Add more android espresso test example

--- app/src/androidTest/java/com/dannyroa/espresso_samples/recyclerview/MainActivityTest.java
+++ app/src/androidTest/java/com/dannyroa/espresso_samples/recyclerview/MainActivityTest.java
@@ -13,14 +13,13 @@ import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
 import androidx.test.platform.app.InstrumentationRegistry;
-import com.dannyroa.espresso_samples.recyclerview.TestUtils.ScrollToPositionViewAction;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
-public class RecyclerViewTest {
+public class MainActivityTest {
 
   @Rule
   public ActivityScenarioRule<MainActivity> activityScenarioRule =
